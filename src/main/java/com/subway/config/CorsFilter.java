a4f472f893136d7715ff2f0f884baf2dff8d742a
plastichong <zakkslash18@naver.com>
2023-09-17 21:32:52 +0900

    refactor: cors Access-Control-Allow-Origin 설정 filter 에서 header set -> config 로 변경

--- src/main/java/com/subway/config/CorsFilter.java
+++ src/main/java/com/subway/config/CorsFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -23,31 +21,32 @@ public class CorsFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//
+//        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+//        httpServletResponse.setHeader("Access-Control-Allow-Methods","*");
+//        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(false);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.setMaxAge(6000L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        if("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+//            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+//        }else {
+//            chain.doFilter(request, response);
+//        }
 
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods","*");
-        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setMaxAge(6000L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-
-        if("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
-            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        }else {
-            chain.doFilter(request, response);
-        }
+        chain.doFilter(request, response);
     }
 
 
