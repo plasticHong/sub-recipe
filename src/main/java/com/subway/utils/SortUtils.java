fee950f657aad7bfb0d314a52783de48d4c7989a
plastichong <zakkslash18@naver.com>
2023-09-18 23:24:00 +0900

    feat: Sort util

---
+++ src/main/java/com/subway/utils/SortUtils.java
@@ -0,0 +1,43 @@
+package com.subway.utils;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SortUtils {
+
+    public Sort getSort(String sortOption, String sortDirection) {
+
+        Sort sortBySortOption = getSortBySortOption(sortOption);
+
+        return setSortDirection(sortBySortOption, sortDirection);
+    }
+
+    private Sort setSortDirection(Sort sort, String sortDirection) {
+
+        if (sortDirection.trim().equalsIgnoreCase("DESC")) {
+            return sort.reverse();
+        }
+
+        return sort;
+    }
+
+    private Sort getSortBySortOption(String sortOption) {
+
+        if (sortOption.trim().equalsIgnoreCase("kcal")) {
+            return Sort.by("kcal");
+        }
+        if (sortOption.trim().equalsIgnoreCase("protein")) {
+            return Sort.by("protein");
+        }
+        if (sortOption.trim().equalsIgnoreCase("fat")) {
+            return Sort.by("fat");
+        }
+        if (sortOption.trim().equalsIgnoreCase("price")) {
+            return Sort.by("price");
+        }
+
+        return Sort.by("id");
+    }
+
+}
